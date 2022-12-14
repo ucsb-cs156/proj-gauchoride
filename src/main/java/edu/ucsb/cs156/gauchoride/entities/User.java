5ffb077e88a44da0f0f255965129c13da7683463
Sebastian Anvari <sebastiananvari@Sebastians-MacBook-Pro-3.local>
2022-12-14 01:16:11 -0800

    driver field initialized to false

--- src/main/java/edu/ucsb/cs156/gauchoride/entities/User.java
+++ src/main/java/edu/ucsb/cs156/gauchoride/entities/User.java
@@ -29,5 +29,6 @@ public class User {
   private String locale;
   private String hostedDomain;
   private boolean admin;
-  private boolean driver;
+@Builder.Default
+  private boolean driver = false;
 }
