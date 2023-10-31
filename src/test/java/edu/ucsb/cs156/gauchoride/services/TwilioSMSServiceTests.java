349e1aa4ffb7d2df6ebba32d87f47dd04de32145
Mohamed-Elfouly <elfouly@ucsb.edu>
2023-10-31 08:34:20 -0700

    Revert "Tried using a twilioClient"
    This reverts commit c2244cc7d0d034493f1661d1997f81f84a62b1f4.

--- src/test/java/edu/ucsb/cs156/gauchoride/services/TwilioSMSServiceTests.java
+++ src/test/java/edu/ucsb/cs156/gauchoride/services/TwilioSMSServiceTests.java
@@ -1,8 +1,6 @@
 package edu.ucsb.cs156.gauchoride.services;
 
 import edu.ucsb.cs156.gauchoride.services.TwilioSMSService;
-import edu.ucsb.cs156.gauchoride.services.TwilioClient;
-
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.rest.api.v2010.account.MessageCreator;
 import com.twilio.Twilio;
@@ -10,17 +8,13 @@ import com.twilio.type.PhoneNumber;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import static org.mockito.ArgumentMatchers.isA;
-import static org.mockito.ArgumentMatchers.any;
 import org.mockito.MockitoAnnotations;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -28,14 +22,12 @@ import static org.mockito.Mockito.doNothing;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.*;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
-@ExtendWith(SpringExtension.class)
 public class TwilioSMSServiceTests {
-
     @Mock
     private MessageCreator messageCreator;
 
@@ -44,38 +36,45 @@ public class TwilioSMSServiceTests {
 
     @Mock
     private Twilio twilio;
-
+    
     @Autowired
     private TwilioSMSService twilioSMSService;
 
-    @MockBean
-    private TwilioClient twilioClient;
+    @Value("${twilio.account.sid:twilio_account_sid_unset}")
+    protected String ACCOUNT_SID = "";
+
+    @Value("${twilio.auth.token:twilio_auth_token_unset}")
+    protected String AUTH_TOKEN = "";
+
+
+    @BeforeEach
+    public void setUp() {
+        //twilioSMSService = new TwilioSMSService();
+        //messageCreator = Mockito.mock(MessageCreator.class);
+        //message = Mockito.mock(Message.class);
+        //when(message.creator(any(PhoneNumber.class), any(PhoneNumber.class), any(String.class))).thenReturn(messageCreator);
+        //when(messageCreator.create()).thenReturn(message);
+        //when(message.getSid()).thenReturn("mocked_sid");
+        //ReflectionTestUtils.setField(twilioSMSService, "messageCreator", messageCreator);
+        MockitoAnnotations.initMocks(this);
+    }
 
     @Test
     public void testSendSMSToAll() {
-        //twilioSMSService = new TwilioSMSService();
-        messageCreator = Mockito.mock(MessageCreator.class);
-        message = Mockito.mock(Message.class);
-
-        when(message.getSid())
-            .thenReturn("MOCK_SID");
-        when(messageCreator.create())
-            .thenReturn(message);
-        when(twilioClient.createMessage(any(), any(), any()))
-            .thenReturn(messageCreator);
-    
-
-        // Define test data
-        Iterable<String> receivers = Arrays.asList("+1234567890", "+9876543210");
+        // Arrange
+        List<String> receivers = Arrays.asList("+1234567890", "+9876543210");
         String content = "Test SMS Content";
+        doNothing().when(twilio).init(isA(String.class), isA(String.class));
+        //when(message.creator(any(PhoneNumber.class), any(PhoneNumber.class), any(String.class))).thenReturn(messageCreator);
+        //when(messageCreator.create()).thenReturn(message);
 
-        // act
+        // Act
         List<String> messageSids = twilioSMSService.sendSMSToAll(receivers, content);
 
-        // Verify
-        assertEquals("MOCK_SID", messageSids.get(0));
-        assertEquals("MOCK_SID", messageSids.get(1));
+        // Assert
+        verify(twilio, times(1)).init(ACCOUNT_SID, AUTH_TOKEN);
+        //assertEquals(2, messageSids.size());
+        //assertEquals("mocked_sid", messageSids.get(0));
+        //assertEquals("mocked_sid", messageSids.get(1));
     }
 }
-
-
