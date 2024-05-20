ff166128a2ca1af2e58b96ee290328fc0fe5b644
Wesley Chiba <wchiba@ucsb.edu>
2024-05-19 17:32:12 -0700

    wc - add more changes for commit info

--- frontend/src/fixtures/systemInfoFixtures.js
+++ frontend/src/fixtures/systemInfoFixtures.js
@@ -1,16 +1,18 @@
 const systemInfoFixtures = {
-    showingBoth:
-    {
-        "springH2ConsoleEnabled": true,
-        "showSwaggerUILink": true
+    showingBoth: {
+      springH2ConsoleEnabled: true,
+      showSwaggerUILink: true,
+      startQtrYYYYQ: "20084",
+      endQtrYYYYQ: "20222",
+      sourceRepo: "mocklink",
     },
-    showingNeither:
-    {
-        "springH2ConsoleEnabled": false,
-        "showSwaggerUILink": false
-    }
-};
-
-
-
+    showingNeither: {
+      springH2ConsoleEnabled: false,
+      showSwaggerUILink: false,
+      startQtrYYYYQ: "20084",
+      endQtrYYYYQ: "20222",
+      sourceRepo: "mocklink",
+    },
+  };
+  
 export { systemInfoFixtures };
