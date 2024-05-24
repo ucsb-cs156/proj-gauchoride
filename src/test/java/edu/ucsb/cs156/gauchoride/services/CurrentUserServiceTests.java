bb54246b1089d03c7e82ac9420d0937a1da3a8ab
Riley Chou <riley33chou@gmail.com>
2024-05-23 18:08:49 -0700

    rc - upgrade spring boot 3

--- src/test/java/edu/ucsb/cs156/gauchoride/services/CurrentUserServiceTests.java
+++ src/test/java/edu/ucsb/cs156/gauchoride/services/CurrentUserServiceTests.java
@@ -10,19 +10,20 @@ import org.junit.jupiter.api.Test;
 import edu.ucsb.cs156.gauchoride.ControllerTestCase;
 import edu.ucsb.cs156.gauchoride.entities.User;
 import edu.ucsb.cs156.gauchoride.services.CurrentUserService;
+import org.mockito.Answers;
 
 class CurrentUserServiceTests extends ControllerTestCase {
 
   @Test
   void test_isLoggedIn_returns_false() {
-    CurrentUserService currentUserService = mock(CurrentUserService.class);
+    CurrentUserService currentUserService = mock(CurrentUserService.class, Answers.CALLS_REAL_METHODS);
     when(currentUserService.getUser()).thenReturn(null);
     assertFalse(currentUserService.isLoggedIn());
   }
 
   @Test
   void test_isLoggedIn_returns_true() {
-    CurrentUserService currentUserService = mock(CurrentUserService.class);
+    CurrentUserService currentUserService = mock(CurrentUserService.class, Answers.CALLS_REAL_METHODS);
     when(currentUserService.getUser()).thenReturn(User.builder().build());
     assertTrue(currentUserService.isLoggedIn());
   }
