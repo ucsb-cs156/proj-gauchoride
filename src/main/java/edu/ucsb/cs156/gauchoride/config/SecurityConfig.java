39331b95bc60e75231eae4a697c62702a8c5a0d6
yuji-sakaguchi <yujisakaguchi@ucsb.edu>
2024-05-26 12:42:36 -0700

    ys - fixing mutation test

--- src/main/java/edu/ucsb/cs156/gauchoride/config/SecurityConfig.java
+++ src/main/java/edu/ucsb/cs156/gauchoride/config/SecurityConfig.java
@@ -137,56 +137,54 @@ public class SecurityConfig {
     Optional<User> u = userRepository.findByEmail(email);
     return u.isPresent() && u.get().getRider();
   }
-  
-}
 
-final class SpaCsrfTokenRequestHandler extends CsrfTokenRequestAttributeHandler {
-  private final CsrfTokenRequestHandler delegate = new XorCsrfTokenRequestAttributeHandler();
+  final class SpaCsrfTokenRequestHandler extends CsrfTokenRequestAttributeHandler {
+    private final CsrfTokenRequestHandler delegate = new XorCsrfTokenRequestAttributeHandler();
 
-  @Override
-  public void handle(HttpServletRequest request, HttpServletResponse response,
-      Supplier<CsrfToken> deferredCsrfToken) {
-    /*
-     * Always use XorCsrfTokenRequestAttributeHandler to provide BREACH protection
-     * of
-     * the CsrfToken when it is rendered in the response body.
-     */
-    this.delegate.handle(request, response, deferredCsrfToken);
-  }
-
-  @Override
-  public String resolveCsrfTokenValue(HttpServletRequest request, CsrfToken csrfToken) {
-    /*
-     * If the request contains a request header, use
-     * CsrfTokenRequestAttributeHandler
-     * to resolve the CsrfToken. This applies when a single-page application
-     * includes
-     * the header value automatically, which was obtained via a cookie containing
-     * the
-     * raw CsrfToken.
-     */
-    if (StringUtils.hasText(request.getHeader(csrfToken.getHeaderName()))) {
-      return super.resolveCsrfTokenValue(request, csrfToken);
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response,
+        Supplier<CsrfToken> deferredCsrfToken) {
+      /*
+       * Always use XorCsrfTokenRequestAttributeHandler to provide BREACH protection
+       * of
+       * the CsrfToken when it is rendered in the response body.
+       */
+      this.delegate.handle(request, response, deferredCsrfToken);
     }
-    /*
-     * In all other cases (e.g. if the request contains a request parameter), use
-     * XorCsrfTokenRequestAttributeHandler to resolve the CsrfToken. This applies
-     * when a server-side rendered form includes the _csrf request parameter as a
-     * hidden input.
-     */
-    return this.delegate.resolveCsrfTokenValue(request, csrfToken);
+
+    @Override
+    public String resolveCsrfTokenValue(HttpServletRequest request, CsrfToken csrfToken) {
+      /*
+       * If the request contains a request header, use
+       * CsrfTokenRequestAttributeHandler
+       * to resolve the CsrfToken. This applies when a single-page application
+       * includes
+       * the header value automatically, which was obtained via a cookie containing
+       * the
+       * raw CsrfToken.
+       */
+      if (StringUtils.hasText(request.getHeader(csrfToken.getHeaderName()))) {
+        return super.resolveCsrfTokenValue(request, csrfToken);
+      }
+      /*
+       * In all other cases (e.g. if the request contains a request parameter), use
+       * XorCsrfTokenRequestAttributeHandler to resolve the CsrfToken. This applies
+       * when a server-side rendered form includes the _csrf request parameter as a
+       * hidden input.
+       */
+      return this.delegate.resolveCsrfTokenValue(request, csrfToken);
+    }
   }
-}
 
-final class CsrfCookieFilter extends OncePerRequestFilter {
+  final class CsrfCookieFilter extends OncePerRequestFilter {
 
-  @Override
-  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-      throws ServletException, IOException {
-    CsrfToken csrfToken = (CsrfToken) request.getAttribute("_csrf");
-    // Render the token value to a cookie by causing the deferred token to be loaded
-    csrfToken.getToken();
-    filterChain.doFilter(request, response);
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+        throws ServletException, IOException {
+      CsrfToken csrfToken = (CsrfToken) request.getAttribute("_csrf");
+      // Render the token value to a cookie by causing the deferred token to be loaded
+      csrfToken.getToken();
+      filterChain.doFilter(request, response);
+    }
   }
-
 }
