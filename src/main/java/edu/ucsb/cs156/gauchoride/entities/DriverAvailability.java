b8b990373930e9ba7391e84fa17376913c512e79
Gen Tamada <gtamada@ucsb.edu>
2024-05-30 01:36:33 -0700

    fixed driver availability

--- src/main/java/edu/ucsb/cs156/gauchoride/entities/DriverAvailability.java
+++ src/main/java/edu/ucsb/cs156/gauchoride/entities/DriverAvailability.java
@@ -1,5 +1,7 @@
 package edu.ucsb.cs156.gauchoride.entities;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -21,7 +23,11 @@ public class DriverAvailability {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
   private long driverId;
+
+  @Schema(allowableValues = "Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday")
+  @Column(name = "\"day\"")
   private String day;
+  
   private String startTime;
   private String endTime;
   private String notes;
