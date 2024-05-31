8e72f5180318c22ff9ac37f740e6ac61a12fdbe6
Gen Tamada <gtamada@ucsb.edu>
2024-05-30 18:37:55 -0700

    fixed lint

--- frontend/src/stories/pages/Scheduler/SchedulerPage.stories.js
+++ frontend/src/stories/pages/Scheduler/SchedulerPage.stories.js
@@ -2,10 +2,6 @@ import React from 'react';
 
 import SchedulerPage from 'main/pages/Scheduler/SchedulerPage';
 
-import { rideFixtures } from 'fixtures/rideFixtures';
-import { driverAvailabilityFixtures } from 'fixtures/driverAvailabilityFixtures';
-import { shiftFixtures } from 'fixtures/shiftFixtures';
-
 export default {
     title: 'pages/Scheduler/SchedulerPage',
     component: SchedulerPage
